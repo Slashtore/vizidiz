502d0c62577b4db56e3c0f0b00e903e12a040474
Slashtore <slashtore@gmail.com>
2026-04-04 11:38:02 +0300

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vizidiz/',  // ← Обязательно!
+  base: '/vizidiz/',  // ← Обязательно (имя репозитория)
   plugins: [react()],
 })
