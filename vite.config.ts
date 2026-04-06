e8d3f2847784c6e9012a54e16c2a9db50130a301
Slashtore <slashtore@gmail.com>
2026-04-06 12:49:36 +0300

    Fix: base path for vizidiz.ru

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vizidiz/',  // ← Обязательно (имя репозитория)
+  base: '/',  // ← Обязательно (имя репозитория)
   plugins: [react()],
 })
